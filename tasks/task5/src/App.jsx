d9105349217222d74a2c8ce3d859a22e84b30af8
Umang Hirani <umanghirani.exe@gmail.com>
2026-02-06 15:28:59 +0530

    css changes

--- tasks/task5/src/App.jsx
+++ tasks/task5/src/App.jsx
@@ -1,22 +1,19 @@
 import "./App.css";
 import CustomRouter from "./components/CustomRouter.jsx";
 import styles from "./app.module.css";
-import { useContext, useState } from "react";
 import { Box } from "@mui/material";
 import FormContextProvider from "./context/FormContext.jsx";
 import StepperComponent from "./components/stepper/stepperComponent.jsx";
+import SuccessModal from "./components/successModal/SuccessModal.jsx";
 
 function App() {
-  const [step, setStep] = useState(0);
-  const value = { step, setStep };
-  const appContext = useContext(value);
-
   return (
     <>
       <Box className={styles.container}>
         <FormContextProvider>
           <StepperComponent />
           <CustomRouter />
+          <SuccessModal />
         </FormContextProvider>
       </Box>
     </>
