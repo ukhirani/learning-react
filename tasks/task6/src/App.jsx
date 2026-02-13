c09ab627101eb4ff8227a65d4d2da6f84edbbb1e
Umang Hirani <umanghirani.exe@gmail.com>
2026-02-13 10:58:36 +0530

    task6 : inital commit

---
+++ tasks/task6/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Box className="">Boilerplate</Box>
+    </>
+  );
+}
+
+export default App;
